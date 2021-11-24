76615806c379445354ee5525b632ccc86ca60de6
Chris's PC <Chris's PC@DESKTOP-GBHC10I.lan>
2021-11-24 01:46:27 -0600

    Group Project Commit

---
+++ groupProject/src/application/SampleController.java
@@ -0,0 +1,37 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class SampleController {
+
+    @FXML
+    private AnchorPane mainScreen;
+
+    @FXML
+    private Button startBtn;
+
+    @FXML
+    private Button leaderBtn;
+
+    @FXML
+    private Button settingsBtn;
+
+    @FXML
+    void startHandle(ActionEvent event) {
+
+    }
+
+    @FXML
+    void leaderHandle(ActionEvent event) {
+
+    }
+
+    @FXML
+    void settingsHandle(ActionEvent event) {
+
+    }
+
+}
